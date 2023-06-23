5df63d9c8ff7f71968be6634379312c27a1e588a
aadhavr <aadhav.rajesh@gmail.com>
2023-06-23 03:56:15 -0700

    Added comments and README

---
+++ README.md
@@ -0,0 +1,53 @@
+# Mean Reversion Strategy
+
+This script implements a mean reversion strategy for a given stock. It calculates the z-scores for the stock's price and generates entry and exit signals based on predefined thresholds. The script also performs a backtest on the strategy and visualizes the returns.
+
+## Disclaimer
+
+Please note that the information provided by this script is for educational and informational purposes only. It should not be considered as financial or investment advice. The strategy implemented in this script may not be suitable for all investors. Investing in the stock market carries inherent risks, and past performance is not indicative of future results. Before making any investment decisions, it is recommended to consult with a qualified financial advisor.
+
+## Prerequisites
+
+Before running this script, ensure that you have the following dependencies installed:
+
+- `pandas`
+- `numpy`
+- `yfinance`
+- `matplotlib`
+- `pandas_datareader`
+
+You can install the dependencies using the following command:
+'''
+pip install pandas numpy yfinance matplotlib pandas_datareader
+'''
+
+## Usage
+
+1. Clone or download the repository to your local machine.
+
+2. Install the required Python libraries mentioned in the prerequisites section.
+
+3. Open the `backtesting_mean_revert.py` file in a text editor.
+
+4. As you see fit, make changes to the thresholds, time frame for the mean calculation, and time frame for the backtesting. (Note: there is a distinct difference in these timeframes to intentionally avoid lookahead bias)
+
+5. Run the `backtesting_mean_revert.py` script using the following command:
+    
+```
+python3 backtesting_mean_revert.py
+```
+6. You will be prompted to enter the stock ticker, time frame for mean calculation, and time frame for backtesting.
+
+7. A graph showing both strategies will be created.
+
+## Note on Lookahead Bias
+
+This script aims to simulate a mean reversion strategy based on historical data while mitigating the risk of lookahead bias. Lookahead bias is a common pitfall in backtesting, where future information is unintentionally incorporated into the development or evaluation of a trading strategy, leading to overly optimistic or unrealistic results.
+
+To address lookahead bias, this script employs the following measures:
+
+   -  Mean Calculation: The mean of the stock's closing prices is calculated using a specific time frame, as provided by the user. This mean calculation is done using historical data up until the given end date, without incorporating any future information. By using historical data, the script aims to capture the underlying trend within the specified time frame.
+
+    - Backtesting: The backtesting of the strategy is performed using a different time frame, also specified by the user. This separation of time frames ensures that the strategy is evaluated based on historical data that is independent of the mean calculation period. The backtesting is conducted using data starting from the end of the mean calculation period until the end date, thus avoiding the use of future information during the testing phase.
+
+By utilizing distinct time frames for mean calculation and backtesting, this script attempts to mitigate the risk of lookahead bias and provide a more realistic assessment of the strategy's performance.
--- backtesting_mean_revert.py
+++ backtesting_mean_revert.py
@@ -6,39 +6,43 @@ from pandas_datareader import data as pdr
 
 yf.pdr_override()
 
-symbol = "AAPL"
-mean_frame = 21  # Time frame for calculating the mean
-backtest_frame = 90  # Time frame for the backtest
+input_symbol = input('Enter stock ticker: ')
+input_mean_frame = input('Enter time frame for mean: ')  # time frame for calculating the mean
+input_backtest_frame = input('Enter time frame for backtest: ')  # time frame for the backtest
 
-start_date = '2018-08-01'
-end_date = '2022-12-31'
-stock_data = pd.DataFrame(yf.download(symbol, start=start_date, end=end_date)['Close'])
+def mean_reversion_strategy(symbol, mean_frame, backtest_frame):
+    start_date = '2018-08-01' # modify this to your liking
+    end_date = '2023-06-23'
+    stock_data = pd.DataFrame(yf.download(symbol, start=start_date, end=end_date)['Close'])
 
-# Calculate the mean for the mean-reversion strategy
-mean_data = stock_data['Close'].rolling(window=mean_frame).mean()
+    # the window is the mean_frame to avoid using redundant data
+    mean_data = stock_data['Close'].rolling(window=mean_frame).mean()
 
-# Calculate the returns and z-scores
-stock_data['returns'] = stock_data['Close'].pct_change()
-stock_data['z_score'] = (stock_data['Close'] - mean_data) / stock_data['Close'].rolling(window=mean_frame).std()
+    # calculates z scores
+    stock_data['returns'] = stock_data['Close'].pct_change()
+    stock_data['z_score'] = (stock_data['Close'] - mean_data) / stock_data['Close'].rolling(window=mean_frame).std()
 
-# Figure out entry and exit thresholds
-percentiles = [5, 10, 50, 90, 95]
-z_scores = stock_data['z_score'].dropna()
-percentile_values = np.percentile(z_scores, percentiles)
+    # figures out entry and exit thresholds
+    percentiles = [5, 10, 50, 90, 95]
+    z_scores = stock_data['z_score'].dropna()
+    percentile_values = np.percentile(z_scores, percentiles)
 
 
-buy = percentile_values[1] # Z-score threshold for entry signal
-sell = percentile_values[3]  # Z-score threshold for exit signal                                         
-stock_data['Signal'] = np.where(stock_data['z_score'] > sell, -1, np.where(stock_data['z_score'] < buy, 1, 0))
+    buy = percentile_values[1] # z-score threshold for entry signal
+    sell = percentile_values[3]  # z-score threshold for exit signal                                         
+    stock_data['Signal'] = np.where(stock_data['z_score'] > sell, -1, np.where(stock_data['z_score'] < buy, 1, 0))
 
-backtest_data = stock_data[-backtest_frame:]
-backtest_data['Signal'] = backtest_data['Signal'].shift(1)  # Shift the signals by one day
-backtest_data['StrategyReturn'] = backtest_data['Signal'] * backtest_data['returns']
-backtest_data['CumulativeReturn'] = (1 + backtest_data['StrategyReturn']).cumprod()
+    backtest_data = stock_data[-backtest_frame:]
+    backtest_data['Signal'] = backtest_data['Signal'].shift(1)  # shifts signals by one day
+    backtest_data['StrategyReturn'] = backtest_data['Signal'] * backtest_data['returns']
+    backtest_data['CumulativeReturn'] = (1 + backtest_data['StrategyReturn']).cumprod()
 
-plt.plot(np.exp(backtest_data['returns'].dropna()).cumprod(), label = "buy, hold")
-plt.plot(np.exp(backtest_data['StrategyReturn'].dropna()).cumprod(), label = "strategy")
-plt.xlabel('Date')
-plt.ylabel('Returns')
-plt.legend()
-plt.show()
+    plt.plot(np.exp(backtest_data['returns'].dropna()).cumprod(), label = "buy, hold")
+    plt.plot(np.exp(backtest_data['StrategyReturn'].dropna()).cumprod(), label = "strategy")
+    plt.xlabel('Date')
+    plt.ylabel('Returns')
+    plt.legend()
+    plt.show()
+
+
+mean_reversion_strategy(input_symbol, int(input_mean_frame), int(input_backtest_frame))
